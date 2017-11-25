e64fa23396aba958ae3bc6a6848ba6de7cb6b7a2
Liz Burton <liz.burton147@gmail.com>
2017-11-25 13:30:36 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,16 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  passengers.each do | suite, name |
+    if name.start_with?("A") && suite == :suite_a
+      return name
+    end
+  end
 end
